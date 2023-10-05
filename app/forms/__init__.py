6dd508fefd8e5efaf435ea9da5c31237238701b8
nickt <nicktan17@gmail.com>
2023-10-04 21:24:43 -0500

    done with models for now

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,6 +1,2 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
-from .business import Business
-from .review import Review
-from .business_category import BusinessCategory
-from .business_category_mapping import BusinessCategoryMapping
