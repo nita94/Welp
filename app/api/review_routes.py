3d63c7b9ee7129ecc866197cff501935950066fc
nickt <nicktan17@gmail.com>
2023-10-09 06:10:18 -0500

    undid logs in review routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -21,12 +21,8 @@ def get_all_reviews(business_id):
     """
     Get all reviews for a business
     """
-    try:
-        reviews = Review.query.filter(Review.business_id == business_id).all()
-        return {'reviews': [review.to_dict() for review in reviews]}
-    except Exception as e:
-        print("Error fetching reviews:", str(e))
-        return {'error': 'Failed to fetch reviews'}, 500
+    reviews = Review.query.filter(Review.business_id == business_id).all()
+    return {'reviews': [review.to_dict() for review in reviews]}
 
 @review_routes.route('/<int:business_id>', methods=['POST'])
 @login_required
@@ -37,20 +33,15 @@ def create_review(business_id):
     form = ReviewForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        try:
-            new_review = Review(
-                content=form.data['content'],
-                user_id=current_user.id,
-                business_id=business_id,
-                rating=form.data['rating']
-            )
-            db.session.add(new_review)
-            db.session.commit()
-            return new_review.to_dict()
-        except Exception as e:
-            print("Error creating review:", str(e))
-            return {'error': 'Failed to create review'}, 500
-
+        new_review = Review(
+            content=form.data['content'],
+            user_id=current_user.id,
+            business_id=business_id,
+            rating=form.data['rating']
+        )
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 @review_routes.route('/<int:review_id>', methods=['PUT'])
@@ -63,15 +54,10 @@ def update_review(review_id):
     form['csrf_token'].data = request.cookies['csrf_token']
     review_to_update = Review.query.get(review_id)
     if form.validate_on_submit():
-        try:
-            review_to_update.content = form.data['content']
-            review_to_update.rating = form.data['rating']
-            db.session.commit()
-            return review_to_update.to_dict()
-        except Exception as e:
-            print("Error updating review:", str(e))
-            return {'error': 'Failed to update review'}, 500
-
+        review_to_update.content = form.data['content']
+        review_to_update.rating = form.data['rating']
+        db.session.commit()
+        return review_to_update.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 @review_routes.route('/<int:review_id>', methods=['DELETE'])
@@ -81,10 +67,6 @@ def delete_review(review_id):
     Delete a review
     """
     review_to_delete = Review.query.get(review_id)
-    try:
-        db.session.delete(review_to_delete)
-        db.session.commit()
-        return {'message': 'Review has been removed'}
-    except Exception as e:
-        print("Error deleting review:", str(e))
-        return {'error': 'Failed to delete review'}, 500
+    db.session.delete(review_to_delete)
+    db.session.commit()
+    return {'message': 'Review has been removed'}
