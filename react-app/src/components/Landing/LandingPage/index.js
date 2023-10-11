0be705cbc55173450a18828159c4ad01b504701d
nickt <nicktan17@gmail.com>
2023-10-10 21:30:14 -0500

    updated styling

--- react-app/src/components/Landing/LandingPage/index.js
+++ react-app/src/components/Landing/LandingPage/index.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getBusinesses } from '../../../store/businesses';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
-import OpenModalButton from '../OpenModalButton';  // Ensure path is correct
+import OpenModalButton from '../OpenModalButton';
+import Footer from '../Footer/Footer'; // Import the Footer component
 
 const AddBusinessModalContent = () => (
   <div>
@@ -25,47 +26,10 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page">
-      <header className="header">
-        <img src="images/welp_logo.png" alt="Welp" className="logo" />
-        <nav className="navigation">
-          <ul>
-            <li>
-              <OpenModalButton 
-                modalComponent={<AddBusinessModalContent />} 
-                buttonText="Welp for Business" 
-              />
-            </li>
-            <li>
-              <button onClick={() => window.location.href='/businesses'}>
-                Write a Review
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      {/* Rest of your LandingPage content */}
       
-      <main className="main-content">
-        <section className="search-bar">
-          <input type="text" placeholder="Search for businesses..." />
-          <button type="button" onClick={() => window.location.href='/search'}>
-            Search
-          </button>
-        </section>
-        
-        <section className="featured-businesses">
-          <h2>Your Next Review Awaits</h2>
-          <div className="business-grid">
-            {businessesArray.map(business => (
-              <article className="business" key={business.id}>
-                <img src={business.image_url} alt={business.name} />
-                <h3>
-                  <Link to={`/businesses/${business.id}`}>{business.name}</Link>
-                </h3>
-              </article>
-            ))}
-          </div>
-        </section>
-      </main>
+      {/* Include the Footer component here */}
+      <Footer />
     </div>
   );
 };
